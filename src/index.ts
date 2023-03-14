de824e80cadca0a7dcdbdea6d6f659cac9a90740
Valerie Nguyen <valerienguyen169@gmail.com>
2023-03-13 22:49:10 -0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,29 @@
-import './config'; // Load environment variables
-import 'express-async-errors'; // Enable default error handling for async errors
+import './config';
+import 'express-async-errors';
+import express, { Express } from 'express';
+
+import {
+  registerUser,
+  logIn,
+  getUserProfileData,
+  getAllUserProfiles,
+  resetProfileViews,
+  updateUserEmail,
+} from './controllers/UserController';
+
+const app: Express = express();
+app.use(express.json());
+
+const { PORT } = process.env;
+
+app.post('/api/users', registerUser); // Create an account
+app.post('/api/login', logIn); // Log in to an account
+app.post('/api/users/profileViews/reset', resetProfileViews);
+
+app.get('/api/users', getAllUserProfiles);
+app.get('/api/users/:userId', getUserProfileData);
+app.post('/api/users/:userId/email', updateUserEmail);
+
+app.listen(PORT, () => {
+  console.log(`Listening at http://localhost:${PORT}`);
+});
