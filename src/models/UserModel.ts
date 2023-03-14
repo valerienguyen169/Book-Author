8a572350d493e9bfa7b505093dbc3348e4bcc2fe
Valerie Nguyen <valerienguyen169@gmail.com>
2023-03-13 22:54:03 -0500

    Add UserModel.ts

---
+++ src/models/UserModel.ts
@@ -0,0 +1,81 @@
+import { AppDataSource } from '../dataSource';
+import { User } from '../entities/User';
+
+const userRepository = AppDataSource.getRepository(User);
+
+async function addUser(email: string, passwordHash: string): Promise<User> {
+  // Create the new user object
+  let newUser = new User();
+  newUser.email = email;
+  newUser.passwordHash = passwordHash;
+
+  newUser = await userRepository.save(newUser);
+
+  return newUser;
+}
+
+async function getUserByEmail(email: string): Promise<User | null> {
+  return await userRepository.findOne({ where: { email } });
+}
+
+async function allUserData(): Promise<User[]> {
+  return await userRepository.find();
+}
+
+async function getUserById(userId: string): Promise<User | null> {
+  const user = await userRepository.findOne({ where: { userId } });
+  return user;
+}
+
+async function getUsersByViews(minViews: number): Promise<User[]> {
+  const users = await userRepository
+    .createQueryBuilder('user')
+    .where('profileViews >= :minViews', { minViews })
+    .select(['user.email', 'user.profileViews', 'user.joinedOn', 'user.userId'])
+    .getMany();
+
+  return users;
+}
+
+async function incrementProfileViews(userData: User): Promise<User> {
+  const updatedUser = userData;
+  updatedUser.profileViews += 1;
+
+  await userRepository
+    .createQueryBuilder()
+    .update(User)
+    .set({ profileViews: updatedUser.profileViews })
+    .where({ userId: updatedUser.userId })
+    .execute();
+
+  return updatedUser;
+}
+
+async function resetAllProfileViews(): Promise<void> {
+  await userRepository
+    .createQueryBuilder()
+    .update(User)
+    .set({ profileViews: 0 })
+    .where('verifiedEmail <> true')
+    .execute();
+}
+
+async function updateEmailAddress(userId: string, newEmail: string): Promise<void> {
+  await userRepository
+    .createQueryBuilder()
+    .update(User)
+    .set({ email: newEmail })
+    .where({ userId })
+    .execute();
+}
+
+export {
+  addUser,
+  getUserByEmail,
+  getUserById,
+  getUsersByViews,
+  incrementProfileViews,
+  allUserData,
+  resetAllProfileViews,
+  updateEmailAddress,
+};
